357ac17c27a6735b595c8ec20d322a3ac2e5f7e9
jhanjhenyi <jhanjhenyi@gmail.com>
2018-10-12 00:32:44 +0800

    Update echo bot function with line bot sdk

--- django_line_bot/echobot/views.py
+++ django_line_bot/echobot/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
+handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+
+
+@csrf_exempt
+def callback(request: HttpRequest) -> HttpResponse:
+    
+    if request.method == "POST":
+        # get X-Line-Signature header value
+        signature = request.META['HTTP_X_LINE_SIGNATURE']
+
+        # get request body as text
+        body = request.body.decode('utf-8')
+
+        # handle webhook body
+        try:
+            handler.handle(body, signature)
+        except InvalidSignatureError:
+            return HttpResponseBadRequest()
+
+        return HttpResponse()
+    else:
+        return HttpResponseBadRequest()
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def message_text(event: MessageEvent):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text)
+    )
