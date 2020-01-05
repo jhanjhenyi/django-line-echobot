877ad94aed1a71adc0a05631e51bc81782aeba03
JhenYi Jhan <jhanjhenyi@gmail.com>
2020-01-05 14:37:30 +0800

    Reinitial project with Django 3.0

--- django_line_bot/config/urls.py
+++ django_line_bot/config/urls.py
@@ -1,7 +1,7 @@
 """django_line_bot URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,9 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('echobot/', include('echobot.urls')),
+    path("admin/", admin.site.urls),
 ]
