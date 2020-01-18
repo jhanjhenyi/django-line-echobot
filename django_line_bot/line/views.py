1c4a16c1e4c491ec3897d75978a9d5bffa1c47e8
JhenYi Jhan <jhanjhenyi@gmail.com>
2020-01-18 15:20:09 +0800

    Added line webhook.

--- django_line_bot/line/views.py
+++ django_line_bot/line/views.py
@@ -1,10 +1,39 @@
+import logging
+
+from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.http.request import HttpRequest
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, HttpResponseBadRequest
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+logger = logging.getLogger("django")
+
+line_bot_api = LineBotApi(settings.CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(settings.CHANNEL_SECRET)
 
 
 @csrf_exempt
 @require_POST
 def webhook(request: HttpRequest):
+    signature = request.headers["X-Line-Signature"]
+    body = request.body.decode()
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        messages = (
+            "Invalid signature. Please check your channel access token/channel secret."
+        )
+        logger.error(messages)
+        return HttpResponseBadRequest(messages)
     return HttpResponse("OK")
+
+
+@handler.add(event=MessageEvent, message=TextMessage)
+def handl_message(event: MessageEvent):
+    line_bot_api.reply_message(
+        reply_token=event.reply_token, messages=TextSendMessage(text=event.message.text)
+    )
